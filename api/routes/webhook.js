945c18431fb4a75010c1f827b74ab955d12c5062
daniloamarante <danilodesouza12@hotmail.com>
2025-06-25 22:46:43 -0300

    fix: estrutura reorganizada com schema.prisma no padrão

--- api/routes/webhook.js
+++ api/routes/webhook.js
@@ -2,8 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const { PrismaClient } = require('../../prisma/generated/client');
-
-
 const prisma = new PrismaClient();
 
 router.post('/woocommerce', async (req, res) => {
