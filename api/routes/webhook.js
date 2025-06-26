9c0a1fa746ceb22c955004363c6a0872f984ca77
daniloamarante <danilodesouza12@hotmail.com>
2025-06-25 22:53:49 -0300

    build: adiciona geração dinâmica do Prisma Client

--- api/routes/webhook.js
+++ api/routes/webhook.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { PrismaClient } = require('../../prisma/generated/client');
+const { PrismaClient } = require('../../../prisma/generated/client');
 const prisma = new PrismaClient();
 
 router.post('/woocommerce', async (req, res) => {
